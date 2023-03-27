9cf721408eb67c9967e324b5f9bf7370e6a8a195
Delta11235 <mateusz._01@wp.pl>
2023-03-27 19:43:11 +0200

    finalnawersjachyba

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function ZmianaSlajdu() {
   let Slajd2 = document.getElementById("Slajd2");
   let Slajd3 = document.getElementById("Slajd3");
   let Slajd4 = document.getElementById("Slajd4");
+  let Slajd5 = document.getElementById("Slajd5");
   liczba++;
-  if (liczba > 4) {
+  if (liczba > 5) {
     liczba = 1;
   }
   if (liczba == 1) {
@@ -14,20 +15,30 @@ function ZmianaSlajdu() {
     Slajd2.style.display = "none";
     Slajd3.style.display = "none";
     Slajd4.style.display = "none";
+    Slajd5.style.display = "none";
   } else if (liczba == 2) {
     Slajd1.style.display = "none";
     Slajd2.style.display = "block";
     Slajd3.style.display = "none";
     Slajd4.style.display = "none";
+    Slajd5.style.display = "none";
   } else if (liczba == 3) {
     Slajd1.style.display = "none";
     Slajd2.style.display = "none";
     Slajd3.style.display = "block";
     Slajd4.style.display = "none";
+    Slajd5.style.display = "none";
   } else if (liczba == 4) {
     Slajd1.style.display = "none";
     Slajd2.style.display = "none";
     Slajd3.style.display = "none";
     Slajd4.style.display = "block";
+    Slajd5.style.display = "none";
+  } else if (liczba == 5) {
+    Slajd1.style.display = "none";
+    Slajd2.style.display = "none";
+    Slajd3.style.display = "none";
+    Slajd4.style.display = "none";
+    Slajd5.style.display = "block";
   }
 }
