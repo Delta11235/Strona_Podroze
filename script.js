6dbc52cb107d5b5367f5c9fe452b63f671138e26
Delta11235 <mateusz._01@wp.pl>
2023-03-27 18:50:44 +0200

    Slajder dziala

---
+++ script.js
@@ -0,0 +1,33 @@
+let liczba = 1;
+
+function ZmianaSlajdu() {
+  let Slajd1 = document.getElementById("Slajd1");
+  let Slajd2 = document.getElementById("Slajd2");
+  let Slajd3 = document.getElementById("Slajd3");
+  let Slajd4 = document.getElementById("Slajd4");
+  liczba++;
+  if (liczba > 4) {
+    liczba = 1;
+  }
+  if (liczba == 1) {
+    Slajd1.style.display = "block";
+    Slajd2.style.display = "none";
+    Slajd3.style.display = "none";
+    Slajd4.style.display = "none";
+  } else if (liczba == 2) {
+    Slajd1.style.display = "none";
+    Slajd2.style.display = "block";
+    Slajd3.style.display = "none";
+    Slajd4.style.display = "none";
+  } else if (liczba == 3) {
+    Slajd1.style.display = "none";
+    Slajd2.style.display = "none";
+    Slajd3.style.display = "block";
+    Slajd4.style.display = "none";
+  } else if (liczba == 4) {
+    Slajd1.style.display = "none";
+    Slajd2.style.display = "none";
+    Slajd3.style.display = "none";
+    Slajd4.style.display = "block";
+  }
+}
